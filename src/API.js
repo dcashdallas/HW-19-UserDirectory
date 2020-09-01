70dd25811f8721179a73fcade8b330133a44bd59
dcashdallas <dcashdallas81@yahoo.com>
2020-09-01 17:46:27 -0500

    heroku deploy

--- src/API.js
+++ src/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const getUsers = function () {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+    return axios.get("https://randomuser.me/api/?results=10&nat=us");
 };
